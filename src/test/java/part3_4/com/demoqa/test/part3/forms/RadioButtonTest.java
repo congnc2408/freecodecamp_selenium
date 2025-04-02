39062836da6b3142295a33497e18b8f15780540e
Nguyễn Chí Công <nccong@vina.moonpo.com>
2025-04-02 16:44:40 +0700

    selenium check rbtn and cbxbtn

---
+++ src/test/java/part3_4/com/demoqa/test/part3/forms/RadioButtonTest.java
@@ -0,0 +1,4 @@
+package part3_4.com.demoqa.test.part3.forms;
+
+public class RadioButtonTest {
+}
---
+++ src/test/java/part3_4/com/demoqa/test/part3/javascript/JavascriptTest.java
@@ -0,0 +1,4 @@
+package part3_4.com.demoqa.test.part3.javascript;
+
+public class JavascriptTest {
+}
